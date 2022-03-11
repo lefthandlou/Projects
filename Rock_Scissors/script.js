d6d2995599c3dc115fa552e9f46008e9dfad70e7
lefthandlou <lefthandlou@pop-os.localdomain>
2022-03-11 10:25:49 -0500

    Working up through the roundPlay function.

--- Rock_Scissors/script.js
+++ Rock_Scissors/script.js
@@ -1,69 +1,30 @@
-
-/*function computerPlay() {
-
+function computerPlay() {
     let randomAttack=['rock' , 'paper' , 'scissors'];
     let computerSelection=randomAttack[Math.floor(Math.random()*randomAttack.length)];
-
-   console.log(computerSelection);
+    console.log(computerSelection);
+    return computerSelection;
 }
-*/
-let playerSelection= 5 ;
-let computerSelection= 5 ;
 
 function playRound (playerSelection, computerSelection) {
-
-    
-    if (playerSelection===computerSelection) {
-        console.log('A tie!');
-      } else {
-        
-      }
-}
-
-
-
-
-/* function playRound(playerSelection, computerSelection) {
-    function assignNumber(playerSelection, computerSelection) {
-        let playerSelection=a;
-        let computerSelection=b;
-        let a=1;
-        let b=2;   
-        let playerWin=(a + b);
-        let computerWin=(a-b);
-    }
-   
-
-    if (playerWin) {
-        return 'You win!'
-    } else if (computerWin) {
-        return 'You lose!';
-    }
-}
-
-
-
-function playRound(playerSelection, computerSelection) {
-
-   if (playerSelection===computerSelection) {
-        return 'Tie';
-
-    } else if ((playerSelection==='rock') && (computerSelection==='scissors') || (playerSelection==='scissors') && (computerSelection==='paper') || (playerSelection==='paper') && (computerSelection==='rock')) {
-        return 'You win!';
-
+    if (playerSelection==computerSelection) {
+        return 'A tie!';
+    } else if ((playerSelection==='rock') && (computerSelection==='scissors') || 
+    (playerSelection==='scissors') && (computerSelection==='paper') || 
+    (playerSelection==='paper') && (computerSelection==='rock')) {
+    return 'You win!';
     } else {
-        return 'You lose!';
-    }    
+    return 'You lose!';
+    }
+}
+
+function game() {
+    for (let i=0; i < 5; i++) {
+        console.log(playRound());
+    }
 }
 
 
-const playerSelection = "rock";
-const computerSelection = computerPlay();
-console.log(playRound(playerSelection, computerSelection)); */
+const playerSelection='rock';
+const computerSelection=computerPlay();
 
-
-
-
-
-
-
+console.log(playRound(playerSelection, computerSelection));
