6f5af49a95da5bca7c02504c6880eed4b5cad044
lefthandlou <lefthandlou@pop-os.localdomain>
2022-03-12 11:06:24 -0500

    Logged five loops of one game round.

--- Rock_Scissors/script.js
+++ Rock_Scissors/script.js
@@ -11,20 +11,22 @@ function playRound (playerSelection, computerSelection) {
     } else if ((playerSelection==='rock') && (computerSelection==='scissors') || 
     (playerSelection==='scissors') && (computerSelection==='paper') || 
     (playerSelection==='paper') && (computerSelection==='rock')) {
-    return 'You win!';
+        return 'You win!';
     } else {
-    return 'You lose!';
+        return 'You lose!';
     }
-}
+}   
 
 function game() {
-    for (let i=0; i < 5; i++) {
-        console.log(playRound());
-    }
+    for (let i = 0; i < 5; i++) {
+        playRound();
+        console.log((playRound(playerSelection, computerSelection)));
+    } 
 }
 
 
 const playerSelection='rock';
 const computerSelection=computerPlay();
 
-console.log(playRound(playerSelection, computerSelection));
+/*console.log(playRound(playerSelection, computerSelection));*/
+console.log(game());
