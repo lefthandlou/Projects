3475b3611c531d0311358182e0e58fc83ca3a95b
lefthandlou <lefthandlou@pop-os.localdomain>
2022-03-14 10:16:45 -0400

    Working fully up to lower case.

--- Rock_Scissors/script.js
+++ Rock_Scissors/script.js
@@ -1,7 +1,7 @@
 function computerPlay() {
     let randomAttack=['rock' , 'paper' , 'scissors'];
     let computerSelection=randomAttack[Math.floor(Math.random()*randomAttack.length)];
-    console.log(computerSelection);
+    console.log('Computer plays ' + computerSelection + ' ');
     return computerSelection;
 }
 
@@ -15,18 +15,33 @@ function playRound (playerSelection, computerSelection) {
     } else {
         return 'You lose!';
     }
+
 }   
 
+
 function game() {
+    let runningScore=0;
     for (let i = 0; i < 5; i++) {
-        playRound();
-        console.log((playRound(playerSelection, computerSelection)));
+        
+        let playerSelection=window.prompt('Enter rock, paper, or scissors');
+        let computerSelection=computerPlay();
+        console.log('Player plays ' + playerSelection + ' ');
+        let roundResult=((playRound(playerSelection, computerSelection)));
+        console.log(roundResult);
+        if (roundResult === 'You win!') {
+            runningScore++;
+        }
+        console.log('Current score:' + runningScore + ' ');
     } 
+    if (runningScore>=3) {
+        console.log('Player wins!');
+    } else {
+        console.log('Computer wins!');
+    }
 }
 
 
-const playerSelection='rock';
-const computerSelection=computerPlay();
+
 
 /*console.log(playRound(playerSelection, computerSelection));*/
 console.log(game());
