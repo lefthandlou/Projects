bc5f0ebf2247483cc51d4be63e26710ecfff0a19
lefthandlou <lefthandlou@pop-os.localdomain>
2022-04-01 14:47:32 -0400

    Make event listener a named function that can be called later.

--- Rock_Scissors/eventListener.js
+++ Rock_Scissors/eventListener.js
@@ -1,11 +1,36 @@
 const buttons = document.getElementsByClassName('selections');
-let btnArray=Array.from(buttons);
 
-btnArray.forEach(button => {
+
+
+/*btnArray.forEach(button => {
 	button.addEventListener('click', function handleClick (event) {
 		console.log('button clicked', event);
 
 		button.classList.add('clicked!');
-	});
-});
+		
+	}); 
+});*/
+
+
+function changeClass() {
+	let btnArray=Array.from(buttons);
+	btnArray.forEach(button => {
+		button.addEventListener('click', function handleClick (event) {
+			console.log('button clicked', event);
+	
+			button.classList.add('clicked!');
+		});
+	}); 
+}
+
+changeClass();
+
+// https://bobbyhadz.com/blog/javascript-add-event-listener-to-all-elements-with-class
+
+
+//Rewrite logic so player selection is based on class names;
+//Add a loop that removes class name of 'clicked!' after round
+
+
+
 
