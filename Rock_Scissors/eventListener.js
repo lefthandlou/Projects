d8b1d9da3175b435187cd8075ad385acf5cffbc5
lefthandlou <lefthandlou@pop-os.localdomain>
2022-04-06 14:44:19 -0400

    All results showing on webpage instead of console

--- Rock_Scissors/eventListener.js
+++ Rock_Scissors/eventListener.js
@@ -1,63 +1,99 @@
 
 const buttons = document.querySelectorAll('.selections');
+let declarePlayer=document.querySelector('.player-play');
+let declareComputer=document.querySelector('.computer-play');
+let resultOfRound=document.querySelector('.results');
+let runScorePlayer=document.querySelector('.player');
+let runScoreComputer=document.querySelector('.computer')
+let gameWinner=document.querySelector('.game-winner');
 let btnArray=Array.from(buttons);
 let playerScore=0;
 let computerScore=0;
-let tie='A tie!';
-let win='You win the round!';
-let lose='You lose the round!';
 
 
 function computerPlay() {
+
     let randomAttack=['rock' , 'paper' , 'scissors'];
-    let computerSelection=randomAttack[Math.floor(Math.random()*randomAttack.length)];
-    console.log('Computer plays ' + computerSelection + '!');
+
+    let computerSelection=randomAttack[Math.floor(Math.random()*
+		randomAttack.length)];
+    
+	declareComputer.textContent=('Computer plays ' + computerSelection + '!');
+	
 	return computerSelection;
 }
 
 
-function playRound () {
+function playRound () {	
+
 	let computerSelection=computerPlay();
 	
 	
-    if ((document.getElementById('rock').classList.contains('clicked!')) && (computerSelection==='rock') || 
-		(document.getElementById('scissors').classList.contains('clicked!')) && (computerSelection==='scissors') ||
-		(document.getElementById('paper').classList.contains('clicked!')) && (computerSelection==='paper')) {
-		return tie;
-	} else if ((document.getElementById('rock').classList.contains('clicked!')) && (computerSelection==='scissors') || 
-		(document.getElementById('scissors').classList.contains('clicked!')) && (computerSelection==='paper') ||
-		(document.getElementById('paper').classList.contains('clicked!')) && (computerSelection==='rock')) {
-        playerScore++;
-		return win;
-    } else if ((document.getElementById('rock').classList.contains('clicked!')) && (computerSelection==='paper') || 
-		(document.getElementById('scissors').classList.contains('clicked!')) && (computerSelection==='rock') ||
-		(document.getElementById('paper').classList.contains('clicked!')) && (computerSelection==='scissors')) {
-        computerScore++
-		return lose;
+    if ((document.getElementById('rock').classList.contains('clicked!')) 		&& 
+			(computerSelection==='rock') 		|| 
+
+		(document.getElementById('scissors').classList.contains('clicked!')) 	&&
+			(computerSelection==='scissors') 	||
+
+		(document.getElementById('paper').classList.contains('clicked!')) 		&&
+			(computerSelection==='paper')) {
+				resultOfRound.textContent=('A tie!');
+	
+				return 'A tie!';
+
+	} else if ((document.getElementById('rock').classList.contains('clicked!')) &&
+			(computerSelection==='scissors') 	|| 
+
+		(document.getElementById('scissors').classList.contains('clicked!')) 	&&
+			(computerSelection==='paper') 		||
+
+		(document.getElementById('paper').classList.contains('clicked!')) 		&&
+			(computerSelection==='rock')) {
+
+				playerScore++;
+				resultOfRound.textContent=('You win the round!');
+				return 'You win the round!';
+
+    } else if ((document.getElementById('rock').classList.contains('clicked!')) &&
+			(computerSelection==='paper') 		|| 
+
+		(document.getElementById('scissors').classList.contains('clicked!')) 	&&
+			(computerSelection==='rock') 		||
+
+		(document.getElementById('paper').classList.contains('clicked!')) 		&&
+			(computerSelection==='scissors')) {
+
+				computerScore++
+				resultOfRound.textContent=('You lose the round!');
+				return 'You lose the round!';
 	};	
 }   
 
 function declarePlay() {
+
 	if ((document.getElementById('rock').classList.contains('clicked!'))) {
-		console.log('Player chose rock!');
+		declarePlayer.textContent='Player chose rock!';
+
 	} else if ((document.getElementById('paper').classList.contains('clicked!'))) {
-		console.log('Player chose paper!');
+		declarePlayer.textContent='Player chose paper!';
 	} else {
-		console.log('Player chose scissors!');
+		declarePlayer.textContent='Player chose scissors!';
 	};
 }
 
 
 function keepScore() {
 	let roundResult=playRound()
-	console.log(roundResult);
+
 	if (playerScore >= 5) {
-		console.log('Player wins the game!')
+		gameWinner.textContent='Player wins the game!'
+
 	} else if (computerScore >= 5) {
-		console.log('Computer wins the game!')
+		gameWinner.textContent='Computer wins the game!'
 	};
-	console.log('Current player score: ' + playerScore);
-	console.log('Current computer score: ' + computerScore);
+
+	runScorePlayer.textContent=('Current player score: ' + playerScore);
+	runScoreComputer.textContent=('Current computer score: ' + computerScore);
 }
 
 
@@ -66,27 +102,19 @@ function playerInput() {
 	
 	btnArray.forEach(button => {
 		button.addEventListener('click', function handleClick (event) {
+
 			button.classList.add('clicked!');
-			//console.log(button.classList);
 
 			console.log(declarePlay());
-			console.log(keepScore());
+
+			console.log(keepScore());	
+		
 			button.classList.remove('clicked!');	
+
 			}); 
 	});
 }
 
 console.log(playerInput());
 
-
-// https://bobbyhadz.com/blog/javascript-add-event-listener-to-all-elements-with-class
-
-/*btnArray.forEach(button => {
-	button.addEventListener('click', function handleClick (event) {
-		console.log('button clicked', event);
-
-		button.classList.add('clicked!');	console.log(computerPlay());
-		
-	}); 
-});*/
 
